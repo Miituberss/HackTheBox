26cd410c165a83c5b817868cfe1dcd98e7f481b9
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2025-10-01 10:33:39 +0200

    Create scrtip-nombres.ps1

---
+++ scrtip-nombres.ps1
@@ -0,0 +1,8 @@
+# Solicitar al usuario que ingrese un string
+$string = Read-Host "Introduce un texto"
+
+# Convertir el texto a minÃºsculas y reemplazar los espacios por guiones
+$resultado = $string.ToLower().Replace(" ", "-")
+
+# Mostrar el resultado
+Write-Host "Resultado: $resultado"
